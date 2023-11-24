a5d070eba1632a0969e30ff35f98ab228edde67b
Tim_o_c <timothy.chhor24@ycdsbk12.ca>
2023-11-24 10:54:08 -0500

    Ed Sheeran having fun in a trampoline park

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,46 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
 	
-	
   /**
-   * Called once at the beginning of execution, put your size all in this method
+   * A sketch that outputs an image of Ed Sheeran moving in a sinosodial wave pattern back and forth across an image of a trampoline
+   * @author: T. Chhor
    */
   public void settings() {
-	// put your size call here
     size(400, 400);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
+  // Declaring images
+  PImage imgEd;
+  PImage imgTrampoline;
+
   public void setup() {
-    background(210, 255, 173);
+    // Loading images
+    imgEd = loadImage("Ed-Sheeran-PNG-High-Quality-Image.png");
+    imgTrampoline = loadImage("OIP (5).jpg");
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
+  // Declaring and initializing variables
+  int intXCoord = -35;
+  int intSpeed = 1;
+
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    // Drawing the trampoline background
+    image(imgTrampoline, 0, 0, 400, 400);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    // Ed Sheeran
+    image(imgEd, intXCoord, (int)(200 + 100 * sin(intXCoord)), 70, 70);
+
+    // A white ball
+    ellipse(intXCoord, 200, 50, 50);
+
+    // Edge detection
+    if (intXCoord > 400 || intXCoord < -35) {
+      intSpeed = intSpeed * -1;
+    }
+    
+    // Movement
+    intXCoord += 10 * intSpeed;
   }
-  
-  // define other methods down here.
 }
